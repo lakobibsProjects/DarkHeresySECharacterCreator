711bcfce1365ea984b8fab51a27a61264ea5e154
lakobib <33352140+lakobib@users.noreply.github.com>
2019-08-08 16:01:49 +0200

    Add class DiceRoller

---
+++ DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/Model/DiceRoller/DiceRoller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkHeresy2CharacterCreator.Model.DiceRoller
+{
+    public static class DiceRoller
+    {
+        static int Roll()
+        {
+            return new Random().Next(1, 101);
+        }
+        static int Roll(int dice)
+        {
+            return new Random().Next(1, dice + 1);
+        }
+    }
+}
