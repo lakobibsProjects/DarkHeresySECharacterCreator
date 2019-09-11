4afa4bc1a87e7e45f1e8e390f2a7dcea2535a945
lakobib <33352140+lakobib@users.noreply.github.com>
2019-09-11 15:38:02 +0200

    Add commands to MainWindowViewModel and bind to MainWindow

--- DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/MainWindowVM.cs
+++ DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/MainWindowVM.cs
@@ -7,21 +7,51 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Drawing;
+using PropertyChanged;
+using System.Windows.Input;
+using DarkHeresy2CharacterCreator.View.CharacterCreationView;
 
 namespace DarkHeresy2CharacterCreator.ViewModel
 {
+    [AddINotifyPropertyChangedInterface]
     public class MainWindowVM
     {
-        public ChangeWindowButtonCommand newButtonCommand;
-
-
-        //Image backgroundImage = Image.FromFile("Res/HorizontalWindowBackground.jpg");
-
+        private readonly DelegateCommand newCharacterCommand;
+        private readonly DelegateCommand closeApplicationCommand;
+        private readonly DelegateCommand loadCharacterCommand;
+        private readonly DelegateCommand deleteCharacterCommand;
+        public ICommand NewCharacterCommand => newCharacterCommand;
+        public ICommand LoadCharacterCommand => loadCharacterCommand;
+        public ICommand DeleteCharacterCommand => deleteCharacterCommand;
+        public ICommand CloseApplicationCommand => closeApplicationCommand;
+                
         public MainWindowVM()
         {
-
+            newCharacterCommand = new DelegateCommand(OnNewCharacter);
+            closeApplicationCommand = new DelegateCommand(OnCloseApplication);
+            deleteCharacterCommand = new DelegateCommand(OnDeleteCaharacter);
+            loadCharacterCommand = new DelegateCommand(OnLoadCharacter);
         }
 
+        private void OnLoadCharacter(object obj)
+        {
+            throw new NotImplementedException();
+        }
 
+        private void OnDeleteCaharacter(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnCloseApplication(object obj)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OnNewCharacter(object obj)
+        {
+            Window homeWorldWindow = new HomeWorld();
+            homeWorldWindow.Show();
+        }
     }
 }
