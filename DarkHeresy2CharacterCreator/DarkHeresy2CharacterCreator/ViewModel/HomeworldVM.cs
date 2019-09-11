b619b528c2f5d229d56e88a9f7730f4b7ac74657
lakobib <33352140+lakobib@users.noreply.github.com>
2019-09-11 16:39:58 +0200

    Add commands to HomeworldVM and bind to appropriate View

--- DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/HomeworldVM.cs
+++ DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/HomeworldVM.cs
@@ -1,14 +1,53 @@
-﻿using System;
+﻿using DarkHeresy2CharacterCreator.View.CharacterCreationView;
+using DarkHeresy2CharacterCreator.ViewModel.Commands;
+using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DarkHeresy2CharacterCreator.ViewModel
 {
-    class HomeworldVM
+    [AddINotifyPropertyChangedInterface]
+    public class HomeworldVM
     {
+        private readonly DelegateCommand nextWindowCommand;
+        private readonly DelegateCommand cancelCommand;
+        private readonly DelegateCommand previousWindowCommand;
+
+        public ICommand NextWindowCommand => nextWindowCommand;
+        public ICommand PreviousWindowCommand => previousWindowCommand;
+        public ICommand CancelCommand => cancelCommand;
+
+        public HomeworldVM()
+        {
+            nextWindowCommand = new DelegateCommand(OnNextWindow);
+            cancelCommand = new DelegateCommand(OnCancel);
+            previousWindowCommand = new DelegateCommand(OnPreviousWindow);
+        }
+
+        private void OnPreviousWindow(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnCancel(object obj)
+        {
+            Window window = obj as Window;
+            window.Close();
+        }
+
+        private void OnNextWindow(object obj)
+        {
+            Window window = obj as Window;
+            window.Close();
+            Window backgroundWindow = new Background();
+            backgroundWindow.Show();
+        }
 
     }
 }
