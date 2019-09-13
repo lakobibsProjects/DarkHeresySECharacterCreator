13e7a80988deff2d506357d4ee560171b2d0a6ae
lakobib <33352140+lakobib@users.noreply.github.com>
2019-09-13 17:25:31 +0200

    Upgrade TODO files

--- DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/obj/Debug/DarkHeresy2CharacterCreator_Content.g.i.cs
+++ DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/obj/Debug/DarkHeresy2CharacterCreator_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("todoviewmodel.txt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("legendtodo.txt")]
 
 
