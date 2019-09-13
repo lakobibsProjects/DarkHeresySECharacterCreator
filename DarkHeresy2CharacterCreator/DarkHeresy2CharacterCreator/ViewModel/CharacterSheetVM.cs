3edf1165f9994264a6a431d042217137396f9787
lakobib <33352140+lakobib@users.noreply.github.com>
2019-09-13 15:06:05 +0200

    Add complete fuctionality to open character in CharacterSheetView

--- DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/CharacterSheetVM.cs
+++ DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/CharacterSheetVM.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DarkHeresy2CharacterCreator.Model.Character;
+using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,16 @@ using System.Threading.Tasks;
 
 namespace DarkHeresy2CharacterCreator.ViewModel
 {
-    class CharacterSheetVM
+    [AddINotifyPropertyChangedInterface]
+    public class CharacterSheetVM
     {
+        #region Properties
+        public ICharacter Character{ get; set; }
+        #endregion
+
+        public CharacterSheetVM()
+        {
+            Character = MainWindowVM.SelectedCharacter;
+        }
     }
 }
