0c0cabfbde7d559996bd43a98681edf1e56160a2
lakobib <33352140+lakobib@users.noreply.github.com>
2019-09-26 21:09:42 +0200

    Complete Role V & VM

--- DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/RoleVM.cs
+++ DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/ViewModel/RoleVM.cs
@@ -28,6 +28,38 @@ namespace DarkHeresy2CharacterCreator.ViewModel
         public ICommand CancelCommand => cancelCommand;
         public DarkHeresy2CharacterCreator.Model.GeneralSuppliment.Role SelectedRole { get; set; }
         public ObservableCollection<DarkHeresy2CharacterCreator.Model.GeneralSuppliment.Role> Roles{ get; set; }
+        public string TalentsString
+        {
+            get
+            {
+                StringBuilder temp = new StringBuilder();
+                if (SelectedRole.RoleTalent.Item2 != null && SelectedRole.RoleTalent.Item1 != null)
+                    temp.Append(SelectedRole.RoleTalent.Item1.ToString()).Append(" or ").Append(SelectedRole.RoleTalent.Item2.ToString());
+                else if (SelectedRole.RoleTalent.Item1 != null)
+                    temp.Append(SelectedRole.RoleTalent.Item1.ToString());
+
+                return temp.ToString().Substring(0, temp.Length);
+            }
+        }
+        public string AptitudesString
+        {
+            get
+            {
+                StringBuilder temp = new StringBuilder();
+
+                if (SelectedRole.Aptitudes.Count > 0)
+                {
+                    foreach (var item in SelectedRole.Aptitudes)
+                    {
+                            if (item.Item2 == null)
+                                temp.Append(item.Item1.ToString()).Append(", ");
+                            else
+                                temp.Append(item.Item1.ToString()).Append(" or ").Append(item.Item2.ToString()).Append(", ");        
+                    }
+                }
+                return temp.ToString().Substring(0, temp.Length - 2);
+            }
+        }
         #endregion
 
         public RoleVM()
