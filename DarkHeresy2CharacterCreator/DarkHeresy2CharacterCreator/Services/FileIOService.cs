0244f6cb7ecbb8cadc3ebe934f47baa4ab8c9310
lakobib <33352140+lakobib@users.noreply.github.com>
2019-10-22 16:46:08 +0200

    Add FileIOService

---
+++ DarkHeresy2CharacterCreator/DarkHeresy2CharacterCreator/Services/FileIOService.cs
@@ -0,0 +1,54 @@
+﻿using DarkHeresy2CharacterCreator.Model.Character;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DarkHeresy2CharacterCreator.Services
+{
+    class FileIOService
+    {
+        private readonly string PATH;
+
+        public BindingList<Character> LoadData()
+        {
+            var fileExists = File.Exists(PATH);
+            if (!fileExists)
+            {
+                File.CreateText(PATH).Dispose();
+                return new BindingList<Character>();
+            }
+            using (var reader = File.OpenText(PATH))
+            {
+                var fileText = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<BindingList<Character>>(fileText);
+            }
+        }
+
+        public void SaveData(BindingList<Character> todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+        public void SaveData(object todoDataList)
+        {
+            using (StreamWriter writer = File.CreateText(PATH))
+            {
+                string output = JsonConvert.SerializeObject(todoDataList);
+                writer.Write(output);
+            }
+        }
+
+        public FileIOService(string path)
+        {
+            PATH = path;
+        }
+    }
+}
